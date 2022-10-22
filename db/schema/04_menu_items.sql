0418abddd083f098104f6fd5451740f4a34dd996
kethnal <kaseyvaldez@hotmail.com>
2022-10-21 18:29:19 -0700

    Create tables for schema

---
+++ db/schema/04_menu_items.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS menu_items CASCADE;
+
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price INTEGER NOT NULL,
+  archive BOOLEAN DEFAULT FALSE
+);
