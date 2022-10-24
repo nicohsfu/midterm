0250d351b401bf2c5d2448e46e7474fad9856f8f
nicohsfu <marcnicohernandez@gmail.com>
2022-10-24 15:48:16 -0700

    resolving branch master merge conflict

--- db/seeds/04_foods.sql
+++ db/seeds/04_foods.sql
@@ -1,17 +1,7 @@
 INSERT INTO foods (name, description, image_url, price)
 VALUES
-<<<<<<< HEAD
-  ("Garlic Bread", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/hmDLU55.png", 899),
-  ("Caesar Salad", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/kngjI4b.png", 1399),
-  ("Scallop Risotto", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/VDWo5CS.png", 2199),
-  ("Carbonara", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/828bfwn.png", 1899),
-  ("Hawaiian Pizza", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/74ujHxf.png", 1799);
-
-
-=======
   ('Garlic Bread', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.', 'https://i.imgur.com/hmDLU55.png', 899),
   ('Caesar Salad', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.', 'https://i.imgur.com/kngjI4b.png', 1399),
   ('Scallop Risotto', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.', 'https://i.imgur.com/VDWo5CS.png', 2199),
   ('Carbonara', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.', 'https://i.imgur.com/828bfwn.png', 1899),
   ('Hawaiian Pizza', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.', 'https://i.imgur.com/74ujHxf.png', 1799);
->>>>>>> queries
