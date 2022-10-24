412df0918b59cc2cc3a51543dad0d919d00928a2
kethnal <kaseyvaldez@hotmail.com>
2022-10-24 12:54:37 -0700

    make seeds for menu items (food)

---
+++ db/seeds/04_foods.sql
@@ -0,0 +1,9 @@
+INSERT INTO foods (name, description, image_url, price)
+VALUES
+  ("Garlic Bread", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/hmDLU55.png", 899),
+  ("Caesar Salad", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/kngjI4b.png", 1399),
+  ("Scallop Risotto", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/VDWo5CS.png", 2199),
+  ("Carbonara", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/828bfwn.png", 1899),
+  ("Hawaiian Pizza", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.", "https://i.imgur.com/74ujHxf.png", 1799);
+
+
