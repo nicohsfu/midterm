b34c7c7c0fa182a99908d8a562749c3fcd2132a4
nicohsfu <marcnicohernandez@gmail.com>
2022-10-24 15:17:35 -0700

    checking routes if we get a hit

--- routes/foods.js
+++ routes/foods.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
-// test routes
-
+// test routes for our 5 EJS templates
 router.get('/1', (req, res) => {
   res.render('admin_foods');
 });
@@ -16,6 +14,11 @@ router.get('/3', (req, res) => {
   res.render('admin_orders');
 });
 
+router.post('/3', (req, res) => {
+  console.log("Place Order button on foods.ejs got clicked!");
+  res.render('admin_orders');
+});
+
 router.get('/4', (req, res) => {
   res.render('foods');
 });
@@ -23,7 +26,6 @@ router.get('/4', (req, res) => {
 router.get('/5', (req, res) => {
   res.render('order');
 });
-
 // -----------
 
 // GET foods/ ✅
@@ -38,59 +40,61 @@ router.get('/admin', (req, res) => {
 
 // POST foods/admin
 router.post('/admin', (req, res) => {
+  console.log("add button on admin_foods.ejs got clicked!");
+  // // move this entire function out of here
+  // // refer to the tweeter project
+  // const addFood = function(food) {
 
-  // move this entire function out of here
-  // refer to the tweeter project
-  const addFood = function(food) {
+  //   // write all queries in separate functions elsewhere, and then call it here
+  //   // don't do it like lines 48/49
 
-    // write all queries in separate functions elsewhere, and then call it here
-    // don't do it like lines 48/49
+  //   const queryString = `
+  //     INSERT INTO foods (name, description, price)
+  //     VALUES ($1, $2, $3)
+  //   `;
 
-    const queryString = `
-      INSERT INTO foods (name, description, price)
-      VALUES ($1, $2, $3)
-    `;
+  //   const queryParams = [`${food.name}`, `${food.description}`, `${food.price}`];
 
-    const queryParams = [`${food.name}`, `${food.description}`, `${food.price}`];
+  //   return pool
+  //     .query(queryString, queryParams)
+  //     .then(result => {
+  //       console.log("result.rows[0]: ", result.rows[0]);
+  //       result.rows[0];
+  //     })
+  //     .catch(error => console.log("error: ", error));
+  // };
 
-    return pool
-      .query(queryString, queryParams)
-      .then(result => {
-        console.log("result.rows[0]: ", result.rows[0]);
-        result.rows[0];
-      })
-      .catch(error => console.log("error: ", error));
-  };
+  // // call the function here, line 66-72 stays here
+  // addFood({ ...req.body })
+  //   .then(food => {
+  //     res.send(food);
+  //   })
+  //   .catch(e => {
+  //     console.error(e);
+  //     res.send(e);
+  //   });
 
-  // call the function here, line 65-72 stays here
-  addFood({ ...req.body })
-    .then(food => {
-      res.send(food);
-    })
-    .catch(e => {
-      console.error(e);
-      res.send(e);
-    });
-
-
-  res.redirect('admin_foods');
+  res.redirect('/foods/admin');
 });
 
 // GET foods/admin/:foodId
 router.get('/admin/:foodId', (req, res) => {
-  res.render('admin_foods');
+  console.log("edit button on admin_foods.ejs got clicked!")
+  res.render('admin_edit');
 });
 
 // PATCH foods/admin/:foodId/edit
 // POST for now though, PATCH as stretch
 router.post('/admin/:foodId/edit', (req, res) => {
-  res.redirect('/admin/:foodId');
+  console.log("save button on admin_edit.ejs was clicked!")
+  res.redirect('/foods/admin');
 });
 
 // DELETE /foods/admin/:foodid/delete
 // POST for now though, DELETE as stretch
 router.post('/admin/:foodId/delete', (req, res) => {
-  res.redirect('admin_foods');
+  console.log("delete button on admin_foods.ejs was clicked!")
+  res.redirect('/foods/admin');
 });
 
 module.exports = router;
--- routes/order_status.js
+++ routes/order_status.js
@@ -3,18 +3,23 @@ const router = express.Router();
 
 // GET order_status/
 router.get('/', (req, res) => {
-  res.render('checkout');
+  res.render('order');
 });
 
 // POST order_status/
 router.post('/', (req, res) => {
-  res.render('checkout');
+  res.redirect('/');
+});
+
+// GET order_status/admin/:order_id/edit
+router.get('/admin/:order_id/edit', (req, res) => {
+  res.render('order');
 });
 
 // PATCH order_status/admin/:order_id/edit
 // POST for now though, PATCH as stretch
 router.post('/admin/:order_id/edit', (req, res) => {
-  res.render('checkout');
+  res.redirect('/admin/:order_id');
 });
 
 module.exports = router;
