108d786e3b348ac57753ec2fd51baab4383f122b
kethnal <kaseyvaldez@hotmail.com>
2022-10-27 01:50:47 -0700

    update lorem ipsum

--- db/seeds/04_seeds.sql
+++ db/seeds/04_seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO foods (name, description, image_url, price)
 VALUES
-  ('Garlic Bread', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nibh nulla, lacinia ut augue sit amet, dapibus convallis velit.', 'https://i.imgur.com/hmDLU55.png', 899),
-  ('Caesar Salad', 'Fusce commodo, urna sit amet viverra euismod, augue enim imperdiet purus, non hendrerit leo erat id ante. Suspendisse potenti.', 'https://i.imgur.com/kngjI4b.png', 1399),
-  ('Scallop Risotto', 'Ut eget ullamcorper nunc. Quisque vel orci magna. Etiam faucibus metus vel sapien malesuada faucibus. Praesent quis velit', 'https://i.imgur.com/VDWo5CS.png', 2199),
-  ('Carbonara', 'Vestibulum nunc elit, blandit a scelerisque at, lobortis volutpat purus. Nam nec iaculis leo, id malesuada risus. Vestibulum.', 'https://i.imgur.com/828bfwn.png', 1899),
-  ('Gourmet Pizza', 'Ut metus felis, pellentesque sed tellus id, pellentesque varius nunc. Etiam consectetur leo eget felis gravida, eget solli.', 'https://i.imgur.com/74ujHxf.png', 1799);
+  ('Gourmet Pizza', 'prosciutto, cremini mushrooms, arugula, basil, 20-year in-house sourdough crust', 'https://i.imgur.com/74ujHxf.png', 1799),
+  ('Carbonara', 'maple smoked pancetta, guanciale, linguine, garlic-parmesan cream sauce, parsley', 'https://i.imgur.com/828bfwn.png', 1899),
+  ('Scallop Risotto', 'brown butter scallops, arborio rice, grana padano, champagne, in-season microgreens', 'https://i.imgur.com/VDWo5CS.png', 2199),
+  ('Caesar Salad', 'spinach & romaine mix, rosemary garlic croutons, free-ranged eggs, avocado slices, house caesar dressing', 'https://i.imgur.com/kngjI4b.png', 1399),
+  ('Garlic Bread', 'rosemary garlic butter, grana padano, parsley, winter truffle, fresh house bagguettes', 'https://i.imgur.com/hmDLU55.png', 899);
